b047ba4c30246fc1e8aef44a26525520d8c9f38a
Mamaev Alexander <fancarster@gmail.com>
2020-12-16 14:43:43 +0300

    added framelogs endpoint and messages

--- go/as/external/api/handyrusty.pb.gw.go
+++ go/as/external/api/handyrusty.pb.gw.go
@@ -134,6 +134,40 @@ func local_request_HandyRustyService_ExecCommand_0(ctx context.Context, marshale
 
 }
 
+func request_HandyRustyService_GetDeviceFramesLog_0(ctx context.Context, marshaler runtime.Marshaler, client HandyRustyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetDeviceFramesLogRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetDeviceFramesLog(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_HandyRustyService_GetDeviceFramesLog_0(ctx context.Context, marshaler runtime.Marshaler, server HandyRustyServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetDeviceFramesLogRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetDeviceFramesLog(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterHandyRustyServiceHandlerServer registers the http handlers for service HandyRustyService to "mux".
 // UnaryRPC     :call HandyRustyServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -196,6 +230,26 @@ func RegisterHandyRustyServiceHandlerServer(ctx context.Context, mux *runtime.Se
 		}
 
 		forward_HandyRustyService_ExecCommand_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_HandyRustyService_GetDeviceFramesLog_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_HandyRustyService_GetDeviceFramesLog_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetDeviceFramesLog_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -300,6 +354,26 @@ func RegisterHandyRustyServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("POST", pattern_HandyRustyService_GetDeviceFramesLog_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_HandyRustyService_GetDeviceFramesLog_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetDeviceFramesLog_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -309,6 +383,8 @@ var (
 	pattern_HandyRustyService_GetFrameSpeed_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "handy-rusty", "frames", "speed"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_HandyRustyService_ExecCommand_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "handy-rusty", "rc", "exec-command"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_HandyRustyService_GetDeviceFramesLog_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "handy-rusty", "frames", "logs", "device-frames"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -317,4 +393,6 @@ var (
 	forward_HandyRustyService_GetFrameSpeed_0 = runtime.ForwardResponseMessage
 
 	forward_HandyRustyService_ExecCommand_0 = runtime.ForwardResponseMessage
+
+	forward_HandyRustyService_GetDeviceFramesLog_0 = runtime.ForwardResponseMessage
 )
