e733ed3730604a7d0a6099f1de701c72decb0757
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2021-08-09 14:19:00 +0300

    build go\swagger

--- go/as/external/api/profiles.pb.go
+++ go/as/external/api/profiles.pb.go
@@ -656,7 +656,9 @@ func init() {
 	proto.RegisterMapType((map[string]string)(nil), "api.DeviceProfile.TagsEntry")
 }
 
-func init() { proto.RegisterFile("as/external/api/profiles.proto", fileDescriptor_a1c507fa3dbc9903) }
+func init() {
+	proto.RegisterFile("as/external/api/profiles.proto", fileDescriptor_a1c507fa3dbc9903)
+}
 
 var fileDescriptor_a1c507fa3dbc9903 = []byte{
 	// 1314 bytes of a gzipped FileDescriptorProto
--- go/as/external/api/serviceProfile.pb.go
+++ go/as/external/api/serviceProfile.pb.go
@@ -639,11 +639,11 @@ var fileDescriptor_4f18fca066634371 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // ServiceProfileServiceClient is the client API for ServiceProfileService service.
 //
@@ -662,10 +662,10 @@ type ServiceProfileServiceClient interface {
 }
 
 type serviceProfileServiceClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
-func NewServiceProfileServiceClient(cc *grpc.ClientConn) ServiceProfileServiceClient {
+func NewServiceProfileServiceClient(cc grpc.ClientConnInterface) ServiceProfileServiceClient {
 	return &serviceProfileServiceClient{cc}
 }
 
