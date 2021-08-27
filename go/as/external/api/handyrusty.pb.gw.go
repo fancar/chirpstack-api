8388ee6cecfff9ea92c2543043addf821e500bb2
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2021-08-27 10:27:11 +0300

    added GetGwStats

--- go/as/external/api/handyrusty.pb.gw.go
+++ go/as/external/api/handyrusty.pb.gw.go
@@ -248,6 +248,39 @@ func local_request_HandyRustyService_GetIntegrationReplies_0(ctx context.Context
 
 }
 
+var (
+	filter_HandyRustyService_GetGwStats_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_HandyRustyService_GetGwStats_0(ctx context.Context, marshaler runtime.Marshaler, client HandyRustyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetGwStatsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_HandyRustyService_GetGwStats_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetGwStats(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_HandyRustyService_GetGwStats_0(ctx context.Context, marshaler runtime.Marshaler, server HandyRustyServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetGwStatsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_HandyRustyService_GetGwStats_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetGwStats(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterHandyRustyServiceHandlerServer registers the http handlers for service HandyRustyService to "mux".
 // UnaryRPC     :call HandyRustyServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -377,6 +410,26 @@ func RegisterHandyRustyServiceHandlerServer(ctx context.Context, mux *runtime.Se
 		}
 
 		forward_HandyRustyService_GetIntegrationReplies_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_HandyRustyService_GetGwStats_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_HandyRustyService_GetGwStats_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetGwStats_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -561,6 +614,26 @@ func RegisterHandyRustyServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("GET", pattern_HandyRustyService_GetGwStats_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_HandyRustyService_GetGwStats_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetGwStats_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -578,6 +651,8 @@ var (
 	pattern_HandyRustyService_GetCurrentState_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "handy-rusty", "current-state"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_HandyRustyService_GetIntegrationReplies_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "handy-rusty", "integration", "replies"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_HandyRustyService_GetGwStats_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"api", "handy-rusty", "gateway", "stats"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -594,4 +669,6 @@ var (
 	forward_HandyRustyService_GetCurrentState_0 = runtime.ForwardResponseMessage
 
 	forward_HandyRustyService_GetIntegrationReplies_0 = runtime.ForwardResponseMessage
+
+	forward_HandyRustyService_GetGwStats_0 = runtime.ForwardResponseMessage
 )
