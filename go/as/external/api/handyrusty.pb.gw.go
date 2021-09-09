24da0196586f9c550783594fd9fc8e504299d87b
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2021-09-09 10:06:16 +0300

    device status methods changed

--- go/as/external/api/handyrusty.pb.gw.go
+++ go/as/external/api/handyrusty.pb.gw.go
@@ -380,6 +380,40 @@ func local_request_HandyRustyService_GetDeviceStatsLastTwoweeksAggregated_0(ctx 
 
 }
 
+func request_HandyRustyService_GetAveragesForDeviceList_0(ctx context.Context, marshaler runtime.Marshaler, client HandyRustyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetAveragesForDeviceListRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetAveragesForDeviceList(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_HandyRustyService_GetAveragesForDeviceList_0(ctx context.Context, marshaler runtime.Marshaler, server HandyRustyServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq handyrusty.GetAveragesForDeviceListRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetAveragesForDeviceList(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterHandyRustyServiceHandlerServer registers the http handlers for service HandyRustyService to "mux".
 // UnaryRPC     :call HandyRustyServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -589,6 +623,26 @@ func RegisterHandyRustyServiceHandlerServer(ctx context.Context, mux *runtime.Se
 		}
 
 		forward_HandyRustyService_GetDeviceStatsLastTwoweeksAggregated_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_HandyRustyService_GetAveragesForDeviceList_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_HandyRustyService_GetAveragesForDeviceList_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetAveragesForDeviceList_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -853,6 +907,26 @@ func RegisterHandyRustyServiceHandlerClient(ctx context.Context, mux *runtime.Se
 
 	})
 
+	mux.Handle("POST", pattern_HandyRustyService_GetAveragesForDeviceList_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_HandyRustyService_GetAveragesForDeviceList_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_HandyRustyService_GetAveragesForDeviceList_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -878,6 +952,8 @@ var (
 	pattern_HandyRustyService_GetDeviceStatsLastTwoweeks_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "handy-rusty", "device", "stats", "last-14-days"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_HandyRustyService_GetDeviceStatsLastTwoweeksAggregated_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"api", "handy-rusty", "device", "stats", "last-14-days-aggregated"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_HandyRustyService_GetAveragesForDeviceList_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 2, 5}, []string{"api", "handy-rusty", "device", "stats", "last-24-hours", "for-list"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -902,4 +978,6 @@ var (
 	forward_HandyRustyService_GetDeviceStatsLastTwoweeks_0 = runtime.ForwardResponseMessage
 
 	forward_HandyRustyService_GetDeviceStatsLastTwoweeksAggregated_0 = runtime.ForwardResponseMessage
+
+	forward_HandyRustyService_GetAveragesForDeviceList_0 = runtime.ForwardResponseMessage
 )
